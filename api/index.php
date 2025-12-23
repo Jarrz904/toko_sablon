ad8491eb31657cf77f5357c07cf00314eaabbbb7
Jarrz904 <mfajarsidik64@gmail.com>
2025-12-23 23:51:53 +0700

    index.php

--- api/index.php
+++ api/index.php
@@ -1,30 +1,3 @@
 <?php
 
-// 1. Buat folder sementara untuk semua kebutuhan Laravel
-$paths = [
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/logs',
-];
-
-foreach ($paths as $path) {
-    if (!is_dir($path)) {
-        mkdir($path, 0755, true);
-    }
-}
-
-// 2. Load aplikasi
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// 3. Paksa Laravel menggunakan folder /tmp secara global
-$app->useStoragePath('/tmp/storage');
-
-// 4. Jalankan aplikasi
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
