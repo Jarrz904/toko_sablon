fb94bda87335bd684085188494711bac3cafa784
Jarrz904 <mfajarsidik64@gmail.com>
2025-12-24 14:23:43 +0700

    Izinkan dan kirim file build aset

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -3,30 +3,17 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\TrimStrings as Middleware;
-use Closure;
-use Illuminate\Support\Facades\Log;
 
 class TrimStrings extends Middleware
 {
+    /**
+     * The names of the attributes that should not be trimmed.
+     *
+     * @var array<int, string>
+     */
     protected $except = [
+        'current_password',
         'password',
         'password_confirmation',
     ];
-
-    public function handle($request, Closure $next)
-    {
-        Log::debug('App\\Http\\Middleware\\TrimStrings: handle() called');
-
-        $input = $request->all();
-
-        array_walk_recursive($input, function (&$value) {
-            if (is_string($value)) {
-                $value = trim($value);
-            }
-        });
-
-        $request->merge($input);
-
-        return $next($request);
-    }
 }
